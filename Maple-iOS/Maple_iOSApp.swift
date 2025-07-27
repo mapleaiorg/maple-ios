4256d7fc16055eafd91384fdbc1350d1b53d19c4
qwy16 <qwy@msn.com>
2025-07-28 02:03:12 +0800

    Initial Commit

---
+++ Maple-iOS/Maple_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maple_iOSApp.swift
+//  Maple-iOS
+//
+//  Created by Wenyan Qin on 2025-07-28.
+//
+
+import SwiftUI
+
+@main
+struct Maple_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
